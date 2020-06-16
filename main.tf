5132b99db2f7a52da9af72c7404a1cd46daf8c44
Sean Sundberg <seansund@us.ibm.com>
2020-06-16 00:23:11 -0500

    Add destroy logic for serviceaccount

--- main.tf
+++ main.tf
@@ -19,10 +19,21 @@ resource "null_resource" "tekton" {
 
   triggers = {
     kubeconfig = var.cluster_config_file_path
+    namespace  = var.tools_namespace
   }
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-tekton.sh"
+
+    environment = {
+      KUBECONFIG = self.triggers.kubeconfig
+      TMP_DIR    = local.tmp_dir
+    }
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "${path.module}/scripts/destroy-tekton.sh ${self.triggers.namespace}"
 
     environment = {
       KUBECONFIG = self.triggers.kubeconfig
