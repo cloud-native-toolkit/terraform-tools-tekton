3b870d05f0d63011274e8d914f3b5c4dcae662e5
Sean Sundberg <seansund@us.ibm.com>
2021-06-22 16:47:51 -0500

    Refactors deploy handling of helm
    - Changes to deploy using helm template from shell
    - Removes helm provider dependency
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ scripts/deploy-tekton.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+NAMESPACE="$1"
+NAME="$2"
+CHART="$3"
+
+HELM=$(command -v helm || command -v ./bin/helm)
+
+if [[ -z "${HELM}" ]]; then
+  curl -Lo helm.tar.gz https://get.helm.sh/helm-v3.6.1-linux-amd64.tar.gz
+  tar xzf helm.tar.gz
+  mkdir -p ./bin && mv ./linux-amd64/helm ./bin/helm
+  rm -rf linux-amd64
+  rm helm.tar.gz
+
+  HELM="$(cd ./bin; pwd -P)/helm"
+fi
+
+${HELM} template "${NAME}" "${CHART}" --namespace "${NAMESPACE}" | \
+  kubectl apply --validate=false -n "${NAMESPACE}" -f -
