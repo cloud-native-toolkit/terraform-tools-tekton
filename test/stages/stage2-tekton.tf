0112e17935929bbfada564fc75a2b28bc9ada870
Sean Sundberg <seansund@us.ibm.com>
2022-03-05 07:43:54 -0600

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-tekton.tf
+++ test/stages/stage2-tekton.tf
@@ -2,7 +2,6 @@ module "dev_tools_tekton" {
   source = "./module"
 
   cluster_config_file_path = module.dev_cluster.config_file_path
-  cluster_type             = module.dev_cluster.platform.type_code
   cluster_ingress_hostname = module.dev_cluster.platform.ingress
   tools_namespace          = module.dev_capture_state.namespace
 }
