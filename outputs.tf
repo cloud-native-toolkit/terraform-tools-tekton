f86ab88f8f4f50abbefa8ecf491d853229cbb779
Sean Sundberg <seansund@us.ibm.com>
2022-05-11 12:22:48 -0500

    Adds tekton interface to module.yaml
    cloud-native-toolkit/terraform-tools-tekton-resources#57
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ output "namespace" {
   value       = var.tekton_dashboard_namespace
   depends_on  = [data.external.tekton_ready]
 }
+
+output "tekton_namespace" {
+  description = "The namespace where Tekton dashboard was deployed"
+  value       = var.tekton_dashboard_namespace
+  depends_on  = [data.external.tekton_ready]
+}
