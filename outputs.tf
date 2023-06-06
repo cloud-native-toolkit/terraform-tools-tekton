3e7b108f20a5f9dd6228e9ab13358ea19387b375
Sean Sundberg <seansund@us.ibm.com>
2023-06-06 11:37:29 -0500

    Add operator_namespace and operator_name outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,18 @@ output "namespace" {
   depends_on  = [null_resource.tekton_ready]
 }
 
+output "operator_namespace" {
+  description = "The namespace where the subscription for the Tekton operator was created"
+  value       = local.operator_namespace
+  depends_on  = [null_resource.tekton_ready]
+}
+
+output "operator_name" {
+  description = "The name of the subscription for the Tekton operator"
+  value       = data.external.get_operator_config.result.packageName
+  depends_on  = [null_resource.tekton_ready]
+}
+
 output "subscription_name" {
   description = "The name of the subscription for the Tekton operator"
   value       = data.external.get_operator_config.result.packageName
